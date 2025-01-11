8efb345450ac67789e99e1c8c7bdeb896e4a26b6
Beyza Yurukakar <beyzayurukakar.p@gmail.com>
2025-01-11 22:33:22 +0300

    feat: added listener middleware

--- src/common/store/configuration.ts
+++ src/common/store/configuration.ts
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './reducers';
+import { listenerMiddleware } from './listenerMiddleware';
 
 // Redux Store configuration
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().prepend(listenerMiddleware.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
---
+++ src/common/store/listenerMiddleware.ts
@@ -0,0 +1,12 @@
+import { addListener, createListenerMiddleware } from '@reduxjs/toolkit';
+import { RootState } from './reducers';
+import { AppDispatch } from './configuration';
+
+export const listenerMiddleware = createListenerMiddleware();
+
+export const startAppListening = listenerMiddleware.startListening.withTypes<
+  RootState,
+  AppDispatch
+>();
+
+export const addAppListener = addListener.withTypes<RootState, AppDispatch>();
