c3c2210f1c562ead376543457c1362f2c29a7638
Beyza Yurukakar <beyzayurukakar.p@gmail.com>
2025-01-12 18:49:00 +0300

    feat: added safeareainsets to floating buttom

--- App.tsx
+++ App.tsx
@@ -3,12 +3,15 @@ import { Navigation } from './src/common/navigation/rootNavigator';
 import Toast from 'react-native-toast-message';
 import { Provider } from 'react-redux';
 import { store } from './src/common/store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Navigation />
-      <Toast config={toastConfig} />
+      <SafeAreaProvider>
+        <Navigation />
+        <Toast config={toastConfig} />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- src/common/components/floating-button/FloatingAddButton.tsx
+++ src/common/components/floating-button/FloatingAddButton.tsx
@@ -4,15 +4,22 @@ import { FontAwesome5 } from '@expo/vector-icons';
 import { floatingAddButtonStyles as styles } from './FloatingAddButton.styles';
 import { dimensions } from '../../styling/dimensions';
 import { COLORS } from '../../styling/colors';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type FloatingAddButtonProps = {
   onPress: () => void;
 };
 const FloatingAddButton: React.FC<FloatingAddButtonProps> = ({ onPress }) => {
+  const { bottom } = useSafeAreaInsets();
   return (
     <TouchableOpacity
       onPress={onPress}
-      style={styles.touchableContainer}
+      style={[
+        styles.touchableContainer,
+        {
+          bottom: styles.touchableContainer.bottom + bottom,
+        },
+      ]}
     >
       <View style={styles.innerContainer}>
         <FontAwesome5
